7f83759b8b7efe757cbcf2964972e46cd4a5b8c2
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-12 18:02:55 +0900

    add : 프로젝트 기본 세팅

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.min.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        loader: "ts-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.(jpg|png|jpeg|bmp|gif|svg)?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.css?$/,
+        loader: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", { legacy: true }],
+              ["@babel/plugin-proposal-class-properties", { loose: true }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./src/assets/favicon.ico",
+      scriptLoading: "defer",
+      inject: "head",
+      cache: true
+    }),
+    new Dotenv({
+      path: path.join(__dirname, "src/.env")
+    }),
+    new webpack.EnvironmentPlugin([])
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    host: "localhost"
+  }
+};
