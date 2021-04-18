7a3a57df554dfb5b02be5d560e912c197d23de70
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-18 23:53:50 +0900

    modify : components lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React, { FC } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { FC, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
+const Main = lazy(() => import("./components/Main"));
+const Movie = lazy(() => import("./components/Movie"));
 
 import "./App.css";
-import { Movie, Main } from "./components";
+// import { Movie, Main } from "./components";
 
 const App: FC<{}> = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/movie" component={Movie} />
-      </Switch>
+      <Suspense fallback={<div>Loadiasdasdasdasdadsadasdasdng...</div>}>
+        <header>
+          <Link to="/">home</Link>
+          <br />
+          <Link to="/movie">movie</Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/movie" component={Movie} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
