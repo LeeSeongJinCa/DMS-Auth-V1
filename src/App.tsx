0aac67be408a9f7af7507bb2293985516e8b5cf2
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 23:44:27 +0900

    update : route Error and Redirect component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 const Main = lazy(() => import("./components/main/Main"));
+const Err = lazy(() => import("./components/login/Error"));
 const Login = lazy(() => import("./components/login/Login"));
 const Agreement = lazy(() => import("./components/login/Agreement"));
 const DashBoard = lazy(() => import("./components/dashboard/Dashboard"));
+const RedirectCode = lazy(
+  () => import("./components/redirectCode/redirectCode")
+);
 
 const App: FC<{}> = () => {
   return (
@@ -14,8 +18,10 @@ const App: FC<{}> = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/error" component={Err} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/dashboard" component={DashBoard} />
+          <Route exact path="/redirect-code" component={RedirectCode} />
           <Route exact path="/privacy-policy-agreement" component={Agreement} />
         </Switch>
       </Suspense>
