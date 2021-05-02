92fe7daae5079d144760b14bc03e79409b3a70d7
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 17:08:20 +0900

    update : route Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React, { FC, Suspense, lazy } from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-
-const Main = lazy(() => import("./components/Main"));
-const Movie = lazy(() => import("./components/Movie"));
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-// import { Movie, Main } from "./components";
+
+const Main = lazy(() => import("./components/main/Main"));
+const Login = lazy(() => import("./components/login/Login"));
 
 const App: FC<{}> = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loadiasdasdasdasdadsadasdasdng...</div>}>
-        <header>
-          <Link to="/">home</Link>
-          <br />
-          <Link to="/movie">movie</Link>
-        </header>
+      <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/movie" component={Movie} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </Suspense>
     </Router>
