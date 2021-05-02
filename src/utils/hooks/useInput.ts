15ae67ced6ccb6004a4dd0e9f87b7eb49e4aa66f
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 23:34:21 +0900

    add : useful hooks

---
+++ src/utils/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import { ChangeEvent, useCallback, useState } from "react";
+
+export type InputElements = HTMLInputElement & HTMLTextAreaElement;
+
+export type OnChangeEvent = ChangeEvent<InputElements>;
+
+const useInput = () => {
+  const [input, setInput] = useState<string>("");
+
+  const onChangeInput = useCallback((e: OnChangeEvent) => {
+    setInput(e.currentTarget.value);
+  }, []);
+
+  return [input, onChangeInput, setInput] as const;
+};
+
+export default useInput;
