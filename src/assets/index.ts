afd8f5e327f680e3150edfee81713d40a85a43e6
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 17:07:42 +0900

    add : add icon and use barrel file for assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,6 @@
 export { default as dmsLogoWhite } from "./dmsLogoWhite.png";
 export { default as dmsLogoMint } from "./dmsLogoMint.svg";
+export { default as keyCheckMint } from "./check.svg";
+export { default as itemDot } from "./dot.svg";
+export { default as loginFace } from "./login-face.svg";
+export { default as loginLock } from "./login-lock.svg";
