ccc6b0000916c2eb3427831132897a76cff4ce9f
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 19:04:31 +0900

    update : add check and unused icon, fix filename

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,8 @@
-export { default as dmsLogoWhite } from "./dmsLogoWhite.png";
-export { default as dmsLogoMint } from "./dmsLogoMint.svg";
-export { default as keyCheckMint } from "./check.svg";
+export { default as dmsLogoWhite } from "./dms-logo-white.png";
+export { default as dmsLogoMint } from "./dms-logo-mint.svg";
+export { default as keyCheckMint } from "./check-mint.svg";
+export { default as keyCheckBlack } from "./check-black.svg";
 export { default as itemDot } from "./dot.svg";
 export { default as loginFace } from "./login-face.svg";
 export { default as loginLock } from "./login-lock.svg";
+export { default as unused } from "./unused-black.svg";
