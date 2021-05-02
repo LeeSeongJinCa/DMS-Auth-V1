a8a3b446d579dd74c847cef2c3f3d232242d9aa9
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-02 23:42:54 +0900

    update : BASE_URL

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "http://192.168.137.1:5000";
 
 export const client = () => {
   const instance = axios.create({
